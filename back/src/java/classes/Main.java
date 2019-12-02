d60a0e1b6927cc3970da45125213d75ca3fc38af
BrenoMaia98 <brenomaia98@gmail.com>
2019-12-01 22:10:33 -0300

    modificação teste de conexão banco

--- back/src/java/classes/Main.java
+++ back/src/java/classes/Main.java
@@ -13,27 +13,27 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException, ParseException, ClassNotFoundException {
 
-		Database db = new Database("127.0.0.1", 3306, "root", "");
-                db.connect("jogodamas");
-		try {
-//                        LocalDate data = LocalDate.now();;
-//                        LocalTime time = LocalTime.now();
-//                        db.inserirRegistroPartida(Date.valueOf(data), Time.valueOf(time) , "Ana", "Breno", "Ana", false);
-                        ResultSet res = db.obterHistorico();  
-                        while (res.next()){
-                            Date data = res.getDate("data");
-                            Time horario = res.getTime("horario");
-                            String jogador_1 = res.getString("jogador_1"),
-                                   jogador_2 = res.getString("jogador_2"),
-                                   vencedor = res.getString("vencedor"),
-                                   empate = res.getString("empate");
-                            System.out.println (" ROW = " + data + ": " + horario + ": " + jogador_1 + ": " + jogador_2 + ": "+ vencedor + ": "+ empate ) ;
-                        }
-                          
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		Database db = new Database("127.0.0.1", 3306, "root", "");
+//                db.connect("jogodamas");
+//		try {
+////                        LocalDate data = LocalDate.now();;
+////                        LocalTime time = LocalTime.now();
+////                        db.inserirRegistroPartida(Date.valueOf(data), Time.valueOf(time) , "Ana", "Breno", "Ana", false);
+//                        ResultSet res = db.obterHistorico();  
+//                        while (res.next()){
+//                            Date data = res.getDate("data");
+//                            Time horario = res.getTime("horario");
+//                            String jogador_1 = res.getString("jogador_1"),
+//                                   jogador_2 = res.getString("jogador_2"),
+//                                   vencedor = res.getString("vencedor"),
+//                                   empate = res.getString("empate");
+//                            System.out.println (" ROW = " + data + ": " + horario + ": " + jogador_1 + ": " + jogador_2 + ": "+ vencedor + ": "+ empate ) ;
+//                        }
+//                          
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 	}
 
