0c2a80cd1f19dc01e29433620f6628a639823545
NaluFigueira <anachfigueira@gmail.com>
2019-12-01 22:48:14 -0300

    exportando db

--- back/jogodamas.sql
+++ back/jogodamas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 02-Dez-2019 às 02:07
+-- Generation Time: 02-Dez-2019 às 02:47
 -- Versão do servidor: 10.1.35-MariaDB
 -- versão do PHP: 7.2.9
 
@@ -37,6 +37,29 @@ CREATE TABLE `classificacao` (
   `saldo_damas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historico`
+--
+
+CREATE TABLE `historico` (
+  `id` int(4) NOT NULL,
+  `data` date NOT NULL,
+  `horario` time NOT NULL,
+  `jogador_1` varchar(50) NOT NULL,
+  `jogador_2` varchar(50) NOT NULL,
+  `vencedor` varchar(50) NOT NULL,
+  `empate` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `historico`
+--
+
+INSERT INTO `historico` (`id`, `data`, `horario`, `jogador_1`, `jogador_2`, `vencedor`, `empate`) VALUES
+(1, '2019-12-01', '17:52:11', 'Ana', 'Breno', 'Ana', '0');
+
 --
 -- Indexes for dumped tables
 --
@@ -46,6 +69,22 @@ CREATE TABLE `classificacao` (
 --
 ALTER TABLE `classificacao`
   ADD PRIMARY KEY (`nome_jogador`);
+
+--
+-- Indexes for table `historico`
+--
+ALTER TABLE `historico`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `historico`
+--
+ALTER TABLE `historico`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
